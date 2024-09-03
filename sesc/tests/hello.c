b08fa3449b92b7d8f39e381fa0fa2aa0c5e62d1e
Derek Rymers <rymersd@gmail.com>
2024-09-02 23:17:54 -0400

    done with prj0

--- sesc/tests/hello.c
+++ sesc/tests/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-	printf("Hi! I am Derek Rymers!!!\n");
+	printf("Hi! I am Derek Rymers and this string is more than 30 characters!!!!\n");
 	return 0;
 }
